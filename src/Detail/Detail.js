d861aa70d67f6f44ed1e4784aceef183bc03c32a
ulrmdhn <auliarama965@gmail.com>
2023-01-25 20:02:15 +0700

    update

--- src/Detail/Detail.js
+++ src/Detail/Detail.js
@@ -1,50 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import './Detail.css'
-import { Await, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Detail.css";
+import { Await, useParams } from "react-router-dom";
 
 function Detail() {
-
-  const {id} = useParams();
-  const [details, setDetail] = useState([])
+  const { id } = useParams();
+  const [details, setDetail] = useState([]);
 
   useEffect(() => {
     axios
       .get(`https://digimon-api.com/api/v1/digimon/${id}`)
-      .then(res => {
-        setDetail(res.data)
+      .then((res) => {
+        setDetail(res.data);
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }, [])
-
-
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   // async function digimon() {
   //   const response = await fetch(`https://digimon-api.com/api/v1/digimon/${id}`);
   //   return await response.json();
   // }
 
-  console.log('details ==> ', details)
-
-  
-
-
+  console.log("details ==> ", details);
 
   return (
     <div>
-    {details.length > 0 ? details.map(detail => (
-      <div key={detail.id} className='detail'>
-        <h1>
-          {detail.name}
-        </h1>
+      <div key={details.id} className="detail">
+        <h1>{details.name}</h1>
+        {details?.images?.map((i) => (
+          <img key={details.id} src={i.href} />
+        ))}
 
-        <img src={detail.images}/>
+        {details?.skills?.filter(s => s.skills == 1).map((s) => (
+          <p>{s.id}</p>
+        ))}
       </div>
-    )): null}
     </div>
-  )
+  );
 }
 
-export default Detail
+export default Detail;
